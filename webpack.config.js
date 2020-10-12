be7b1c9e2dd0fcfb9bc4966e01fd649715840cb6
Tyler <tylergrinn@gmail.com>
2020-10-11 22:18:16 -0400

    Copy all files in 'src/favicon' and 'src/img' to public folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/js/index.js',
@@ -7,5 +8,13 @@ module.exports = {
     path: path.join(__dirname, 'public'),
     filename: '[name].[contenthash].js',
   },
-  plugins: [new CleanWebpackPlugin()],
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyPlugin({
+      patterns: [
+        { from: 'src/img', to: 'img' },
+        { from: 'src/favicon', to: 'favicon' },
+      ],
+    }),
+  ],
 };
